fb75ec512cbc913a9546d42ae6844b189e258099
SabinaGarayeva <sgarayeva13@gmail.com>
2024-07-05 22:46:39 +0100

    setting up project

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
+
+export default function Header() {
+  return (
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
+      <SearchOrder />
+     <Username/>
+    </header>
+  );
+}
